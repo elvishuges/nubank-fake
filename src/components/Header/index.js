96bcb8f23616b6e8cae094a820811be7d601f3f9
Elvis <elvishuges@hotmail.com>
2020-07-06 00:15:42 -0300

    add header com icon e nome de usuario

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,39 @@
 import React from "react";
-import { Container } from "./styles";
+import { Text } from "react-native";
+import { StyleSheet,Image } from 'react-native';
+
+import {
+    Container, Top, Logo, Title,
+  } from './styles';
+import logo from './../../assets/Nubank_Logo.png'
+import Icon  from "react-native-vector-icons/MaterialIcons";
 
 export default function Header() {
+    const uriImage = 'https://media.istockphoto.com/vectors/supermarket-set-products-icon-vector-id852099060'
     return(
-      <Container/>
-    );
+      <Container>
+          <Top>  
+              <Logo source={logo}/>            
+              <Title><Text>Elvis Huges</Text></Title>
+          </Top>
+          <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
+      </Container>
+    )
 }
+
+const styles = StyleSheet.create({
+  
+    avatar: {
+      width: 130,
+      height: 130,
+      borderRadius: 63,
+      borderWidth: 4,
+      borderColor: "white",
+      marginBottom:50,
+      alignSelf:'center',
+      position: 'absolute',
+      marginTop:40
+    },
+    
+  });
+  
